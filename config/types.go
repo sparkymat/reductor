d7abfd5a4cfe406a78d9fc526ea5f7fdf5538419
Ajith Hussain <csy0013@googlemail.com>
2018-02-08 17:14:21 +1100

    Removing unused fields

--- config/types.go
+++ config/types.go
@@ -3,12 +3,10 @@ package config
 type HeadConfig struct {
 	JavascriptIncludes []string `toml:"javascript_includes"`
 	CssIncludes        []string `toml:"css_includes"`
-	RawTags            []string `toml:"raw_tags"`
 }
 
 type BodyConfig struct {
-	AppId string   `toml:"app_id"`
-	Nodes []string `toml:"nodes"`
+	AppId string `toml:"app_id"`
 }
 
 type Type struct {
